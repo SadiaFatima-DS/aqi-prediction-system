204eead02f7888f42e965e4e97da8715aa92abda
SadiaFatima-DS <sadia.fatima677@gmail.com>
2025-07-20 22:16:50 +0500

    BUGFIX IN YAML TO UPLOAD TO DATA.

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -7,15 +7,15 @@ import json
 # -----------------------------
 # Configuration
 # -----------------------------
-SERVICE_ACCOUNT_FILE = json.loads(os.environ['SERVICE_ACCOUNT_JSON'])
+SERVICE_ACCOUNT_INFO = json.loads(os.environ['SERVICE_ACCOUNT_JSON'])
 SCOPES = ['https://www.googleapis.com/auth/drive']
 CSV_FILE = 'lahore_aqi_iqair.csv'
 FOLDER_ID = '100x3cRbI-JzrNAKjhBrnnYR_5ZUktJQZ'
 
 def upload_to_drive():
     try:
-        creds = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE, scopes=SCOPES
+        creds = service_account.Credentials.from_service_account_info(
+            SERVICE_ACCOUNT_INFO, scopes=SCOPES
         )
         service = build('drive', 'v3', credentials=creds)
 
@@ -30,7 +30,6 @@ def upload_to_drive():
         media_body = MediaFileUpload(CSV_FILE, resumable=True, mimetype='text/csv')
 
         if files:
-            # File exists, update it
             file_id = files[0]['id']
             service.files().update(fileId=file_id, media_body=media_body).execute()
             print(f"Updated existing file: {CSV_FILE}")
